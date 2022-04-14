d1cfc9e73531e5ff7952939acec4a61c4a4d4dae
Andrei <andrisrom@gmail.com>
2022-04-14 21:13:02 +0300

    add readers

--- src/by/epam/tasks/task2/Main.java
+++ src/by/epam/tasks/task2/Main.java
@@ -2,6 +2,5 @@ package by.epam.tasks.task2;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
---
+++ src/by/epam/tasks/task3/until/FileReader.java
@@ -0,0 +1,17 @@
+package by.epam.tasks.task3.until;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+    private String path;
+
+    public FileReader(String path) {
+        this.path = path;
+    }
+
+    public List<String> readFile (){
+        String temp = path;
+        return new ArrayList<String>();
+    }
+}
