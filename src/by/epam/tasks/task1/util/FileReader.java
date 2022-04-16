6bbc7a67b70895ca3670409c4fcdcdb9e3bf02ea
Andrei <andrisrom@gmail.com>
2022-04-16 22:41:47 +0300

    add try-with-resources fix REGEX

--- src/by/epam/tasks/task1/util/FileReader.java
+++ src/by/epam/tasks/task1/util/FileReader.java
@@ -2,7 +2,6 @@ package by.epam.tasks.task1.util;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,22 +12,16 @@ public class FileReader {
         FileValidator validator = new FileValidator();
         List<String> lines = new ArrayList<>();
         if (validator.isFileExistStream(fileName)) {
-            Scanner scanner = null;
-            try {
-                String root = System.getProperty("user.dir");
-                String filePath = root + File.separator + "data" + File.separator + fileName;
-
-                File file = new File(filePath);
-                scanner = new Scanner(file);
+            String root = System.getProperty("user.dir");
+            String filePath = root + File.separator + "data" + File.separator + fileName;
+            File file = new File(filePath);
+            try (Scanner scanner = new Scanner(file)){
+                while (scanner.hasNext()) {
+                    String line = scanner.nextLine();
+                    lines.add(line);
+                }
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
-            } finally {
-                assert scanner != null;
-                scanner.close();
-            }
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-                lines.add(line);
             }
         }
         return lines;
--- src/by/epam/tasks/task2/util/reader/CustomArrayReader.java
+++ src/by/epam/tasks/task2/util/reader/CustomArrayReader.java
@@ -3,10 +3,12 @@ package by.epam.tasks.task2.util.reader;
 import by.epam.tasks.task1.util.FileReader;
 import by.epam.tasks.task2.entity.CustomArray;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class CustomArrayReader {
+
+  // any positive and negative integers, zero, excluding -0
+    public static final String REGEX = "-?[1-9]\\d*|0";
 
     public CustomArray getArrayFromFile(String fileName) {
         CustomArray customArray = new CustomArray();
@@ -16,11 +18,11 @@ public class CustomArrayReader {
             String[] numbers = s.split(",");
             for (String number : numbers) {
                 String num = number.trim();
-                if (num.matches("T(-{0,1}(?!0)\\d+)")) {
+                if (num.matches(REGEX)) {
                     customArray.add(Integer.parseInt(num));
                 }
             }
         }
-        return new CustomArray();
+        return customArray;
     }
 }
